641500e2accc92c61ed86f2ff65f1216e484a9ed
Kashin <guptakashin@gmail.com>
2022-04-15 00:03:19 +0530

    day3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_catalog/home_page.dart';
-
+import 'package:flutter_catalog/pages/login_page.dart';
+import 'pages/home_page.dart';
 void main() {
   runApp(MyApp());
 }
@@ -11,7 +11,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      // home: HomePage(), //either use line 23 code or this
+      darkTheme: ThemeData(
+        // brightness: Brightness.light,
+        primarySwatch: Colors.cyan
+      ),
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      themeMode: ThemeMode.dark,
+
+      //ROUTES
+      initialRoute: "/",
+      routes: {
+        "/": (context) => HomePage(),
+        "/login": (context) => LoginPage()
+      },
     );
   }
 }
